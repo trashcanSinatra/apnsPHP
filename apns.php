a3458cfc84c5fdc1163d33379be4e9a9613deadc
Aaron Vogt <atvogt@iupui.edu>
2015-05-03 21:10:54 -0400

    Added 2nd GET handler to call final push

--- apns.php
+++ apns.php
@@ -5,23 +5,35 @@ include('_classes/apns.class.php');
 include('_classes/debug.class.php');
 include('_classes/getDebug.class.php');
 
+$db = new dataLink();
+$aspn = new apns($db);
+$push = new push($aspn, true);
+
 
 if (isset($_GET['msg']) & isset($_GET['type'])) {
 
 	$type = $_GET['type'];
-	$msg = $_GET['msg'];
-	$db = new dataLink();
-	$aspn = new apns($db);
-	$push = new push($aspn, true);
+	$typeArray = array('flush', 'store', 'store_push');
+	
+		if(!in_array($type, $typeArray)) {
+			exit();
+		}	
+		$msg = $_GET['msg'];
+		$response = $push->delegatePush($type, $msg);
+		echo $response;
 
+} elseif (isset($_GET['pushCode'])) {
 	
-	$response = $push->delegatePush($type, $msg);
-	echo $response;	
-	
+	$pushCode = $_GET['pushCode'];
+	if($pushCode != "49 5c a4 f7 78 a2 4f d4 99 a4") {	exit(); }
+
+		$xmlResponse = $push->finalPush();
+		echo $xmlResponse;
+		exit();
+
 } else {
 	exit();
 }
 
-?>
-}
+
 ?>
